8ef9969202bf39ef1d95fda9d432cb15f12e02fb
Baz1ngaa <danila.buyval@gmail.com>
2023-03-14 20:40:40 +0100

    new

--- DBanalyiy.py
+++ DBanalyiy.py
@@ -6,14 +6,16 @@ sql.execute("""CREATE TABLE IF NOT EXISTS profileTel (
     region TEXT,
     klass TEXT)""")
 db.commit()
-user_id="121"
-user_region="europa"
-user_klass="11"
+#user_id="121"
+#user_region="europa"
+#user_klass="11"
 
 #sql.execute(f"SELECT login FROM profileTel WHERE login = '{user_id}'")
 #if sql.fetchone() is None:
-    #sql.execute(f"INSERT INTO profileTel VALUES (?,?,?)", (user_id, user_region, user_klass))
-    #db.commit()
+ #   sql.execute(f"INSERT INTO profileTel VALUES (?,?,?)", (user_id, user_region, user_klass))
+  #  db.commit()
+sql.execute("DELETE FROM profileTel WHERE login=121")
+db.commit()
 
 for value in sql.execute("SELECT * FROM profileTel"):
     print(value)
