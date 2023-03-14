eafe96c94eb91e4a5fc5af9538d9e9e364b504f1
Baz1ngaa <danila.buyval@gmail.com>
2023-03-14 20:34:01 +0100

    new

--- DBanalyiy.py
+++ DBanalyiy.py
@@ -10,12 +10,12 @@ user_id="121"
 user_region="europa"
 user_klass="11"
 
-sql.execute(f"SELECT login FROM profileTel WHERE login = '{user_id}'")
-if sql.fetchone() is None:
-    sql.execute(f"INSERT INTO profileTel VALUES (?,?,?)", (user_id, user_region, user_klass))
-    db.commit()
+#sql.execute(f"SELECT login FROM profileTel WHERE login = '{user_id}'")
+#if sql.fetchone() is None:
+    #sql.execute(f"INSERT INTO profileTel VALUES (?,?,?)", (user_id, user_region, user_klass))
+    #db.commit()
 
 for value in sql.execute("SELECT * FROM profileTel"):
     print(value)
-print("dad")
+#print("dad")
 #qsq
